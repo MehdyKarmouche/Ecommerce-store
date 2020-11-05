242e391e7b3a70ae271c80a50f19a6048307a934
Mehdi Karmouche <mehdi.karmouche@gmail.com>
2020-11-05 00:10:49 +0000

    Fetch all products

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const products = require('../products')
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/api/products', (req, res, next)=> {
+  console.log("Ok")
+  res.json(products)
+});
+
+router.get('/api/products/:id', (req, res, next) => {
+  const product = products.find(p => p._id === req.params.id)
+  res.json(product)
 });
 
 module.exports = router;
