69f96a7ec8ef1043012e1a3892b0a98729f8515e
Mehdi Karmouche <mehdi.karmouche@gmail.com>
2020-11-20 13:14:38 +0000

    add fake user models

---
+++ backend/users.js
@@ -0,0 +1,22 @@
+const bcrypt = require('bcryptjs')
+
+const users = [
+  {
+    name: 'Admin User',
+    email: 'admin@gmail.com',
+    password: bcrypt.hashSync('password12', 10),
+    isAdmin: true,
+  },
+  {
+    name: 'Flaming potatoes',
+    email: 'flaming@gmail.com',
+    password: bcrypt.hashSync('password12', 10),
+  },
+  {
+    name: 'Fuzzy Booleans',
+    email: 'fuzzy@gmail.com',
+    password: bcrypt.hashSync('password12', 10),
+  },
+]
+
+export default users
