49430ddf718fc46fe5b310d27f8569a747025be4
Mehdi Karmouche <mehdi.karmouche@gmail.com>
2020-11-20 18:13:34 +0000

    fix: typo in imports (composeWithDevTools)

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,6 +1,6 @@
 import {createStore, combineReducers, applyMiddleware, compose} from 'redux'
 import thunk from 'redux-thunk'
-import{ composeWithDevtools} from 'redux-devtools-extension'
+import{ composeWithDevTools} from 'redux-devtools-extension'
 
 const reducer = combineReducers({})
 
@@ -8,7 +8,7 @@ const initialState = {}
 
 const middleware = [thunk]
 
-const store = createStore(reducer, initialState, composeWithDevtools(applyMiddleware(...middleware)))
+const store = createStore(reducer, initialState, composeWithDevTools(applyMiddleware(...middleware)))
 
 
 export default  store;
