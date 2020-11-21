d190431f9b95170f5dba67df69a81f795faf3147
Mehdi Karmouche <mehdi.karmouche@gmail.com>
2020-11-21 21:06:52 +0000

    feat: add acrtAction and dispatch the cartReducer

---
+++ frontend/src/actions/cartActions.js
@@ -0,0 +1,18 @@
+import axios from 'axios'
+import {CARD_ADD_ITEM, CART_ADD_ITEM} from '../constants/cartConstants'
+
+export const addToCart = {id, qty} =>  async(dispatch, getState) => {
+    const {data} = await axios.get(`http://localhost:5000/api/products/${id}`)
+    dispatch ({
+        type: CARD_ADD_ITEM,
+        payload: {
+            product: data._id,
+            name: data.name,
+            image: data.image,
+            price: data.price,
+            countInStock: data.countInStock,
+            qty:qty
+        }
+    })
+    localStorage.setItem('cartItems',JSON.stringify(getState().cart.cartItems))
+}
