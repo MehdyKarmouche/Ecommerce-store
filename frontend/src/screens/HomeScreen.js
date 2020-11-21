373e3a6571c236743a136d015292afa389ebb1d1
Mehdi Karmouche <mehdi.karmouche@gmail.com>
2020-11-21 16:48:25 +0000

    feat: dispatch action from component HomeScreen

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,9 +1,11 @@
 import React, {useState, useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
 //import products from '../products'
 import { makeStyles } from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
 import Container from '@material-ui/core/Container'
 import Product from '../components/Product'
+import {listProducts} from '../actions/productActions'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -19,24 +21,24 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const HomeScreen = () => {
-    const [products,setProducts] = useState([])
     const classes = useStyles();
 
-    async function fetchProducts(){
-      const res = await fetch("http://localhost:5000/api/products")
-      
-      res.json().then(res => setProducts(res))
-      
-  
-  
-    }
+    const dispatch = useDispatch()
+
+    const productList = useSelector(state => state.productList)
+    const {loading, error, products} = productList
+
+    
     useEffect(() => {
-      fetchProducts()
-    },[])
+      dispatch(listProducts())
+    },[dispatch])
+
+    
     
     return (
     <div className={classes.root}>
         <Container>
+          
         <Grid  container spacing={3}>
             {products.map((product)=> (
                 <Grid key={product._id} item xs={12} sm={6} md={4}>
@@ -44,6 +46,7 @@ const HomeScreen = () => {
                 </Grid>
             ))}
         </Grid>
+        
         </Container>
     </div>
     )
