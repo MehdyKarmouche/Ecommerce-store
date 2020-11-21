7958a35589123774e3f26a62cc93cbc7649d75c6
Mehdi Karmouche <mehdi.karmouche@gmail.com>
2020-11-21 21:04:55 +0000

    debug: remove typos from constants

--- frontend/src/constants/cartConstants.js
+++ frontend/src/constants/cartConstants.js
@@ -1,2 +1,2 @@
-export const CARD_ADD_ITEM = 'CARD_ADD_ITEM'
-export const CARD_REMOVE_ITEM = 'CARD_REMOVE_ITEM'
+export const CART_ADD_ITEM = 'CART_ADD_ITEM'
+export const CART_REMOVE_ITEM = 'CART_REMOVE_ITEM'
