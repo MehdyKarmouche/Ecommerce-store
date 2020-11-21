03fbf3e4bdf603def91ae88552d04cacdff41f7d
Mehdi Karmouche <mehdi.karmouche@gmail.com>
2020-11-21 16:48:45 +0000

    refactor: switch to axios

--- frontend/src/actions/productActions.js
+++ frontend/src/actions/productActions.js
@@ -1,10 +1,11 @@
 import {PRODUCT_LIST_REQUEST,PRODUCT_LIST_SUCCESS,PRODUCT_LIST_FAIL} from '../constants/productContants'
+import axios from 'axios'
 
-export const listProduct = () => async(dispatch) => {
+export const listProducts = () => async(dispatch) => {
     try {
         dispatch({type:PRODUCT_LIST_REQUEST})
-        const {data} = await fetch('api/products')
-
+        const {data} = await axios.get('http://localhost:5000/api/products')
+        
         dispatch({type: PRODUCT_LIST_SUCCESS, payload: data})
 
     }
