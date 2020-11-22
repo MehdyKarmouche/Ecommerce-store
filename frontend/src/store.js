4e61b4373fd19d4956575e871b9b210f36c20fc4
Mehdi Karmouche <mehdi.karmouche@gmail.com>
2020-11-22 21:26:01 +0000

    feat: update redux store

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,16 +3,21 @@ import thunk from 'redux-thunk'
 import{ composeWithDevTools} from 'redux-devtools-extension'
 import {productListReducer, productDetailsReducer} from './reducers/productReducers'
 import {cartReducer} from './reducers/cartReducers'
+import {userLoginReducer} from './reducers/userReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    userLogin: userLoginReducer
 })
 const cartItemsFromLocalStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
 
+const userInfoFromLocalStorage = localStorage.getItem('userInfo') ? JSON.parse(localStorage.getItem('userInfo')) : null
+
 const initialState = {
-    cart : { cartItems: cartItemsFromLocalStorage}
+    cart : { cartItems: cartItemsFromLocalStorage},
+    userLogin : {userInfo:userInfoFromLocalStorage}
 }
 
 const middleware = [thunk]
