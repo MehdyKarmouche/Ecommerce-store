4d17e7e136a8f2e693492e48c6ef3f93c82dafa6
Mehdi Karmouche <mehdi.karmouche@gmail.com>
2020-11-23 17:24:13 +0000

    feat: add signup UI screen

---
+++ frontend/src/screens/RegisterScreen.js
@@ -0,0 +1,160 @@
+import React, {useState, useEffect} from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import { Link } from 'react-router-dom'
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import {register} from '../actions/userActions'
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      marginTop: theme.spacing(8),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+
+const RegisterScreen = ({location, history}) => {
+    const classes = useStyles();
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [name, setName] = useState('')
+    const [message,setMessage] = useState(null)
+
+    const dispatch = useDispatch()
+
+    const userRegister = useSelector(state => state.userLogin)
+
+    const {loading,error,userInfo} = userRegister
+
+    const redirect = location.search ? location.search.split('=')[1] : '/'
+    console.log(redirect)
+    const submitHandler = (e) => {
+        e.preventDefault()
+        if(password !== confirmPassword){
+            setMessage('Passwords not matching')
+        } else {
+            dispatch(register(email,password,name))
+        }
+        
+        
+    }
+    useEffect(() => {
+        if(userInfo){
+            history.push(redirect)
+        }
+
+    },[history,userInfo, redirect])
+
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+        <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign up
+        </Typography>
+        
+        {error && <Message error={error}/>}
+        {loading && <Loader/>}
+        <form onSubmit={submitHandler} className={classes.form} noValidate>
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            value={name}
+            label="name"
+            id="name"
+            autoFocus
+            onChange={(e) => setName(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            value={email}
+            label="Email Address"
+            autoComplete="email"
+            id="email"
+            autoFocus
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            value={password}
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            value={confirmPassword}
+            label="Confirm password"
+            type="password"
+            id="confirmPassword"
+            autoComplete="current-password"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          />
+          <FormControlLabel
+            control={<Checkbox value="remember" color="primary" />}
+            label="Remember me"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+          >
+            Sign Up
+          </Button>
+          <Grid container>
+            <Grid item>
+              <Link to={redirect ? `/login?redirect=${redirect}` : '/login'}>
+                Do you have an account? Sign In
+              </Link>
+            </Grid>
+          </Grid>
+        </form>
+      </div>
+    </Container>
+  );
+}
+
+export default RegisterScreen
