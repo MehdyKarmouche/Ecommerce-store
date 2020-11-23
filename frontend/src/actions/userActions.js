418ceaf9b4062153dd202028e9c5e2296f3a1c43
Mehdi Karmouche <mehdi.karmouche@gmail.com>
2020-11-23 17:23:39 +0000

    feat: add reducer action

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -1,4 +1,4 @@
-import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT } from "../constants/userConstants"
+import { USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS } from "../constants/userConstants"
 import axios from 'axios'
 
 export const login = (email,password) => async (dispatch) => {
@@ -32,4 +32,37 @@ export const login = (email,password) => async (dispatch) => {
 export const logout = () => (dispatch) => {
     localStorage.removeItem('userInfo')
     dispatch({type:USER_LOGOUT})
+}
+
+export const register = (email,password,name) => async (dispatch) => {
+    try {
+        dispatch({
+            type: USER_REGISTER_REQUEST
+        })
+
+        const config = {
+            headers: {
+                'Content-Type':'application/json'
+            }
+        }
+        const {data} = await axios.post('api/users', {email, password, name},config)
+        
+
+        dispatch({
+            type: USER_REGISTER_SUCCESS,
+            payload: data
+        })
+        dispatch({
+            type: USER_LOGIN_REQUEST,
+            payload: data
+        })
+        localStorage.setItem('userInfo',JSON.stringify(data))
+    } catch (error) {
+        dispatch({
+            type:USER_REGISTER_FAIL,
+            payload: error.response && error.response.data.message
+                ?error.response.data.message
+                :error.message,
+        })
+    }
 }
