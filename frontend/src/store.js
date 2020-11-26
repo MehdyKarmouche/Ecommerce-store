87884a2c0849de40f1f696912e373ca8aabbcdfd
Mehdi Karmouche <mehdi.karmouche@gmail.com>
2020-11-26 16:27:24 +0000

    feat: define new constants for profile details

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,13 +5,15 @@ import {productListReducer, productDetailsReducer} from './reducers/productReduc
 import {cartReducer} from './reducers/cartReducers'
 import {userLoginReducer} from './reducers/userReducers'
 import {userRegisterReducer} from './reducers/userReducers'
+import {userDetailsReducer} from './reducers/userReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
-    userRegister: userRegisterReducer
+    userRegister: userRegisterReducer,
+    userDetails: userDetailsReducer
 })
 const cartItemsFromLocalStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
 
