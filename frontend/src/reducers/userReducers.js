b6cba0d637a0505eb152abc98ff1baf9f47f98c2
Mehdi Karmouche <mehdi.karmouche@gmail.com>
2020-11-26 21:47:41 +0000

    feat: add reducer for updating user info

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,4 +1,4 @@
-import { USER_DETAILS_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS } from "../constants/userConstants"
+import { USER_DETAILS_FAIL, USER_DETAILS_REQUEST, USER_DETAILS_SUCCESS, USER_LOGIN_FAIL, USER_LOGIN_REQUEST, USER_LOGIN_SUCCESS, USER_LOGOUT, USER_REGISTER_FAIL, USER_REGISTER_REQUEST, USER_REGISTER_SUCCESS, USER_UPDATE_PROFILE_FAIL, USER_UPDATE_PROFILE_REQUEST, USER_UPDATE_PROFILE_RESET, USER_UPDATE_PROFILE_SUCCESS } from "../constants/userConstants"
 
 export const userLoginReducer = (state = {}, action) => {
     switch (action.type){
@@ -36,6 +36,21 @@ export const userDetailsReducer = (state = {user: {}}, action) => {
             return {loading:false, user:action.payload}
         case USER_DETAILS_FAIL:
             return {loading: false, error:action.payload}
+        default:
+            return state
+    }
+}
+
+export const userUpdateProfileReducer = (state = {}, action) => {
+    switch (action.type){
+        case USER_UPDATE_PROFILE_REQUEST:
+            return {loading:true}
+        case USER_UPDATE_PROFILE_SUCCESS:
+            return {loading:false, success:true, userInfo:action.payload}
+        case USER_UPDATE_PROFILE_FAIL:
+            return {loading: false, error:action.payload}
+        case USER_UPDATE_PROFILE_RESET:
+            return {}
         default:
             return state
     }
