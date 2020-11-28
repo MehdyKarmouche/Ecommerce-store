6242b9aa807c620faf898a17ebe020cff588d07c
Mehdi Karmouche <mehdi.karmouche@gmail.com>
2020-11-28 21:16:42 +0000

    feat: getorder details by id id frontend

--- frontend/src/reducers/orderReducers.js
+++ frontend/src/reducers/orderReducers.js
@@ -1,7 +1,10 @@
 import {
     ORDER_CREATE_REQUEST,
     ORDER_CREATE_SUCCESS,
-    ORDER_CREATE_FAIL
+    ORDER_CREATE_FAIL,
+    ORDER_DETAILS_REQUEST,
+    ORDER_DETAILS_SUCCESS,
+    ORDER_DETAILS_FAIL
     }
     from '../constants/orderConstants'
 
@@ -18,6 +21,28 @@ export const orderCreateReducer = (state = {}, action) => {
                 order: action.payload
             }
         case ORDER_CREATE_FAIL:
+            return {
+                loading: false,
+                error:action.payload
+            }
+        default:
+            return state   
+    }
+}
+
+export const orderDetailsReducer = (state = {orderItems: [], shippingAddress: {}}, action) => {
+    switch(action.type){
+        case ORDER_DETAILS_REQUEST:
+            return {
+                state,
+                loading: true
+            }
+        case ORDER_DETAILS_SUCCESS:
+            return {
+                loading: false,
+                order: action.payload
+            }
+        case ORDER_DETAILS_FAIL:
             return {
                 loading: false,
                 error:action.payload
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,7 +7,7 @@ import {userLoginReducer} from './reducers/userReducers'
 import {userRegisterReducer} from './reducers/userReducers'
 import {userDetailsReducer} from './reducers/userReducers'
 import {userUpdateProfileReducer} from './reducers/userReducers'
-import {orderCreateReducer} from './reducers/orderReducers'
+import {orderCreateReducer, orderDetailsReducer} from './reducers/orderReducers'
 
 const reducer = combineReducers({
     productList: productListReducer,
@@ -17,7 +17,8 @@ const reducer = combineReducers({
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
-    orderCreate: orderCreateReducer
+    orderCreate: orderCreateReducer,
+    orderDetails: orderDetailsReducer
 })
 const cartItemsFromLocalStorage = localStorage.getItem('cartItems') ? JSON.parse(localStorage.getItem('cartItems')) : []
 
