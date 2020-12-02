bb88dcf0c207ee114f5fb8f4ddfa146477146293
Mehdi Karmouche <mehdi.karmouche@gmail.com>
2020-12-02 12:53:25 +0000

    feat: delete a product and update state

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,7 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
 import{ composeWithDevTools} from 'redux-devtools-extension'
-import {productListReducer, productDetailsReducer} from './reducers/productReducers'
+import {productListReducer, productDetailsReducer, productDeleteReducer} from './reducers/productReducers'
 import {cartReducer} from './reducers/cartReducers'
 import {userLoginReducer} from './reducers/userReducers'
 import {userRegisterReducer} from './reducers/userReducers'
@@ -18,6 +18,7 @@ import {orderListMyReducer} from './reducers/orderReducers'
 const reducer = combineReducers({
     productList: productListReducer,
     productDetails: productDetailsReducer,
+    productDelete: productDeleteReducer,
     cart: cartReducer,
     userLogin: userLoginReducer,
     userRegister: userRegisterReducer,
