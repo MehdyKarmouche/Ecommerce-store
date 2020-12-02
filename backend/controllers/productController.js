93f6d28fcd77b138f9aa49786b61d4a028b36e14
Mehdi Karmouche <mehdi.karmouche@gmail.com>
2020-12-02 13:30:38 +0000

    feat: add create & update product controller for admin only

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -30,8 +30,61 @@ const deleteProduct = asynchHandler(async(req, res) => {
         res.status(404).json({message:"Sorry, product not found"})
 })
 
+const createProduct = asynchHandler(async (req, res) => {
+    const product = new Product({
+      name: 'Sample name',
+      price: 0,
+      user: req.user._id,
+      image: '/images/sample.jpg',
+      brand: 'Sample brand',
+      category: 'Sample category',
+      countInStock: 0,
+      numReviews: 0,
+      description: 'Sample description',
+    })
+  
+    const createdProduct = await product.save()
+    res.status(201).json(createdProduct)
+  })
+
+//Update a product 
+
+
+const updateProduct = asynchHandler(async (req, res) => {
+    const {
+      name,
+      price,
+      description,
+      image,
+      brand,
+      category,
+      countInStock,
+    } = req.body
+  
+    const product = await Product.findById(req.params.id)
+  
+    if (product) {
+      product.name = name
+      product.price = price
+      product.description = description
+      product.image = image
+      product.brand = brand
+      product.category = category
+      product.countInStock = countInStock
+  
+      const updatedProduct = await product.save()
+      res.json(updatedProduct)
+    } else {
+      res.status(404)
+      throw new Error('Product not found')
+    }
+  })
+
+
 module.exports = {
     getProducts,
     getProductById,
-    deleteProduct
+    deleteProduct,
+    createProduct,
+    updateProduct
 }
--- backend/routes/productRouter.js
+++ backend/routes/productRouter.js
@@ -1,17 +1,17 @@
 var express = require('express');
-var {getProducts, getProductById, deleteProduct} = require("../controllers/productController")
+var {getProducts, getProductById, deleteProduct, createProduct, updateProduct} = require("../controllers/productController")
 const protect = require('../middleware/authMiddleware')
 const admin = require('../middleware/adminMiddleware')
 var router = express.Router();
 
 
 
-router.route('/').get(getProducts)
+router.route('/').get(getProducts).post(protect, admin, createProduct)
 
 
 //get one product
 
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct)
+router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct).put(protect, admin,updateProduct)
 
 
 
