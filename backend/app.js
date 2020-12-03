8c6035c5192b29b9608b7eae1924acd35594a504
Mehdi Karmouche <mehdi.karmouche@gmail.com>
2020-12-03 19:40:23 +0000

    feat: add image upload endpoint with multer

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,7 @@ const dotenv = require("dotenv").config();
 var productRouter = require('./routes/productRouter');
 var userRouter = require('./routes/userRouter');
 var orderRouter = require('./routes/orderRouter');
+var uploadRouter = require('./routes/uploadRouter')
 
 
 var app = express();
@@ -30,10 +31,14 @@ app.use(cors())
 app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
+app.use('/api/upload', uploadRouter)
 
 app.get('/api/config/paypal',(req,res) => {
   res.send(process.env.PAYPAL_CLIENT_ID)
 })
+
+
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 
 // catch 404 and forward to error handler
